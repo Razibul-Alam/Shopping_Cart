3b8d685995974768655dfc4abdd1b04a47ca5f11
Razibul-Alam <Razibul-Alam@users.noreply.github.com>
2021-01-27 19:55:39 +0600

    plus button done

---
+++ index.js
@@ -0,0 +1,23 @@
+// plus button action
+var plusbtn=document.getElementById('plusbtn');
+plusbtn.addEventListener('click',function(){
+    var inputvalue=document.getElementById('inpt').value;
+   inputvalue=parseFloat(inputvalue);
+   var plusplus=inputvalue+1;
+   var inputvalue=document.getElementById('inpt').value=plusplus;
+   var plustext=document.getElementById('plustext').innerText;
+   plustext=parseFloat(plustext);
+   var totalamount=plustext*2;
+   var plustext=document.getElementById('plustext').innerText=totalamount;
+   
+
+   
+
+   
+})///
+
+// minus button action
+var minusbtn=document.getElementById('minusbtn');
+minusbtn.addEventListener('click',function(){
+    
+})
